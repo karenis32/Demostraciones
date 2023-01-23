fe922e857cb6bd0f56d2cc2009dd78981ac03f68
karenis32 <karen.is32@hotmail.com>
2023-01-23 20:35:50 -0300

    Agrego proyecto WebApiPubs

---
+++ Demostraciones/WebApiNorthwind/Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebApiNorthwind.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            InverseReportsToNavigation = new HashSet<Employee>();
+            Orders = new HashSet<Orders>();
+        }
+
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Title { get; set; }
+        public string TitleOfCourtesy { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string HomePhone { get; set; }
+        public string Extension { get; set; }
+        public byte[] Photo { get; set; }
+        public string Notes { get; set; }
+        public int? ReportsTo { get; set; }
+        public string PhotoPath { get; set; }
+
+        public virtual Employee ReportsToNavigation { get; set; }
+        public virtual ICollection<Employee> InverseReportsToNavigation { get; set; }
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
---
+++ Demostraciones/WebApiPubs/Controllers/PublisherController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiPubs.Models;
+
+namespace WebApiPubs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+
+        private readonly pubsContext context;
+
+        public PublisherController(pubsContext context)
+        {
+
+            this.context = context;
+
+        }
+
+        //GET: api/publisher
+        [HttpGet]
+        public ActionResult<IEnumerable<Publisher>> Get()
+        {
+            return context.Publishers.ToList();
+        }
+
+
+        // GET api/publisher/5
+        [HttpGet("{id}")]
+        public ActionResult<Publisher> GetbyId(string id)
+        {
+            Publisher publisher = (from a in context.Publishers
+                           where a.PubId == id
+                           select a).SingleOrDefault();
+
+            return publisher;
+
+        }
+
+
+        //POST api/publisher
+        [HttpPost]
+        public ActionResult Post(Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Publishers.Add(publisher);
+            context.SaveChanges();
+            return Ok();
+        }
+
+
+        //UPDATE
+        // PUT api/publisher/2
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Publisher publisher)
+        {
+            if (id != publisher.PubId)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(publisher).State = EntityState.Modified;
+            context.SaveChanges();
+
+            return Ok();
+
+        }
+
+        //DELETE api/publisher/2
+        [HttpDelete]
+        public ActionResult<Publisher> Delete(Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            context.Publishers.Remove(publisher);
+            context.SaveChanges();
+
+            return publisher;
+        }
+
+    }
+}
